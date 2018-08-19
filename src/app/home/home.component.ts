8b171356f969e80d5314750e14f669a602ad135c
parshva45 <parshva45@gmail.com>
2018-08-19 18:55:55 +0530

    graph done for static data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -35,7 +35,7 @@ export class HomeComponent implements OnInit {
       'time' : 'x1_10',
       'PowerFactor' : 0.939866,
       'kWh' : 11.309999,
-      'month' : 1,
+      'month' : 2,
       'year' : 2014,
       'day' : 1,
       'hour' : 1,
@@ -49,7 +49,7 @@ export class HomeComponent implements OnInit {
       'time' : 'x1_15',
       'PowerFactor' : 0.936801,
       'kWh' : 12.21,
-      'month' : 1,
+      'month' : 3,
       'year' : 2014,
       'day' : 1,
       'hour' : 1,
@@ -63,9 +63,9 @@ export class HomeComponent implements OnInit {
       'time' : 'x1_20',
       'PowerFactor' : 0.938343,
       'kWh' : 11.4,
-      'month' : 1,
+      'month' : 3,
       'year' : 2014,
-      'day' : 1,
+      'day' : 10,
       'hour' : 1,
       'minute' : 20
     },
@@ -77,9 +77,9 @@ export class HomeComponent implements OnInit {
       'time' : 'x1_35',
       'PowerFactor' : 0.939161,
       'kWh' : 11.73,
-      'month' : 1,
+      'month' : 6,
       'year' : 2014,
-      'day' : 1,
+      'day' : 10,
       'hour' : 1,
       'minute' : 35
     },
@@ -91,7 +91,7 @@ export class HomeComponent implements OnInit {
       'time' : 'x1_40',
       'PowerFactor' : 0.93623,
       'kWh' : 11.67,
-      'month' : 1,
+      'month' : 7,
       'year' : 2014,
       'day' : 1,
       'hour' : 1,
@@ -105,7 +105,7 @@ export class HomeComponent implements OnInit {
       'time' : 'x1_45',
       'PowerFactor' : 0.939433,
       'kWh' : 11.429999,
-      'month' : 1,
+      'month' : 8,
       'year' : 2014,
       'day' : 1,
       'hour' : 1,
@@ -119,7 +119,7 @@ export class HomeComponent implements OnInit {
       'time' : 'x1_50',
       'PowerFactor' : 0.938343,
       'kWh' : 11.969999,
-      'month' : 1,
+      'month' : 12,
       'year' : 2014,
       'day' : 1,
       'hour' : 1,
@@ -127,43 +127,40 @@ export class HomeComponent implements OnInit {
     }
   ];
 
+  kWhData = [];
+
   ngOnInit() {
+
+    for (let i = 0; i < this.courtData.length; i++) {
+      this.kWhData.push({
+        x: new Date(this.courtData[i]['year'],
+          this.courtData[i]['month'] - 1,
+          this.courtData[i]['day'],
+          this.courtData[i]['hour'],
+          this.courtData[i]['minute']),
+        y: this.courtData[i]['kWh']
+      });
+    }
+
     this.lineChart = new Chart('lineChart', {
       type: 'line',
       data: {
-        labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
         datasets: [{
-          data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
-          label: 'Africa',
+          data: this.kWhData,
+          label: 'kWh',
           borderColor: '#3e95cd',
           fill: false
-        },  {
-          data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
-          label: 'Asia',
-          borderColor: '#8e5ea2',
-          fill: false
-        },  {
-          data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
-          label: 'Europe',
-          borderColor: '#3cba9f',
-          fill: false
-        }, {
-          data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
-          label: 'Latin America',
-          borderColor: '#e8c3b9',
-          fill: false
-        }, {
-          data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
-          label: 'North America',
-          borderColor: '#c45850',
-          fill: false
-        }
-        ]
+        }]
       },
       options: {
         title: {
           display: true,
-          text: 'World population per region (in millions)'
+          text: 'Pattern of kWh over time'
+        },
+        scales: {
+          xAxes: [{
+            type: 'time'
+          }]
         }
       }
     });
