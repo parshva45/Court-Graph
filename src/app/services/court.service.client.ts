9c2599ade1c5a90b314d4857aa97b86ab138af57
parshva45 <parshva45@gmail.com>
2018-08-21 01:26:21 +0530

    updating files for deployment

--- src/app/services/court.service.client.ts
+++ src/app/services/court.service.client.ts
@@ -1,5 +1,6 @@
 export class CourtServiceClient {
-  COURT_URL = 'http://localhost:4000/api/court';
+  // COURT_URL = 'http://localhost:4000/api/court';
+  COURT_URL = 'https://court-node-server.herokuapp.com/api/court';
   findAllCourts() {
     return fetch(this.COURT_URL)
       .then(response => response.json());
